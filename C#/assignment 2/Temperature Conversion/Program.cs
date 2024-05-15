fff404263aeb50de82560741f4c69f955eb651b6
RosslerGKBoquiren <142700435+RosslerGKBoquiren@users.noreply.github.com>
2024-05-15 00:21:00 -0400

    Add files via upload

---
+++ C#/assignment 2/Temperature Conversion/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace TemperatureConversion
+{
+    class Program
+    {
+        const double ToFahrenheitFormula = 9.00 / 5.00;
+        const double ToCelsiusFormula = 5.00 / 9.00;
+        const double TemperatureFactor = 32.00;
+        static void Main(string[] args)
+        {
+            /* const double ToFahrenheitFormula = 9.00 / 5.00;
+            const double ToCelsiusFormula = 5.00 / 9.00;
+            const double TemperatureFactor = 32.00; */
+
+            Console.Write("Please enter a temperature in Celsius: ");
+            double celsiusInput = Convert.ToDouble(Console.ReadLine());
+            double fahrenheitOutput = CelsiusToFahrenheit(celsiusInput);
+            Console.WriteLine("The temperature {0} Celsius is equivalent to {1} Fahrenheit", celsiusInput, fahrenheitOutput);
+
+            Console.WriteLine("Please enter a temperature in Fahrenheit: ");
+            double fahrenheitInput = Convert.ToDouble(Console.ReadLine());
+            double celsiusOutput = FahrenheitToCelsius(fahrenheitInput);
+            Console.WriteLine("The temperature {0} Fahrenheit is equivalent to {1} Celsius", fahrenheitInput, celsiusOutput);
+        }
+        static double CelsiusToFahrenheit(double celsiusInput)
+        {
+            double inFahrenheit = celsiusInput * ToFahrenheitFormula + TemperatureFactor;
+            return inFahrenheit;
+        }
+
+        static double FahrenheitToCelsius(double fahrenheitInput)
+        {
+            double inCelsius = Math.Round((fahrenheitInput - TemperatureFactor) * ToCelsiusFormula);
+            return inCelsius;
+        }
+    }
+}
+
