913f004b13fe6e715f41dfcfbb473944980e1d82
RosslerGKBoquiren <142700435+RosslerGKBoquiren@users.noreply.github.com>
2024-05-17 00:14:03 -0400

    Add files via upload

---
+++ C#/week 1/assignment 2/Weight Conversion/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace WeightConversion
+{
+    class Program
+    {
+       const double conversionFactor = 2.2;
+       static void Main(string[] args) 
+        {
+            Console.Write("Enter a weight in kilograms: ");
+            double kilogramsInput = Convert.ToDouble(Console.ReadLine());
+            double poundsOutput = KilogramToPounds(kilogramsInput);
+            Console.WriteLine("The weight {0} kilograms is equivalent to {1} pounds.", kilogramsInput, poundsOutput);
+
+            Console.Write("Enter a weight in pounds: ");
+            double poundsInput = Convert.ToDouble(Console.ReadLine());
+            double kilogramsOutput = PoundsToKilograms(poundsInput);
+            Console.WriteLine("The weight {0} pounds is equivalent to {1} kilograms.", poundsInput, kilogramsOutput);
+        }
+
+        static double KilogramToPounds(double kilogramsInput) 
+        {
+            double inPounds = kilogramsInput * conversionFactor;
+            return inPounds;
+        }
+
+        static double PoundsToKilograms(double poundsInput)
+        {
+            double inKilograms = Math.Round(poundsInput / conversionFactor);
+            return inKilograms;
+        }
+            
+    }
+}
