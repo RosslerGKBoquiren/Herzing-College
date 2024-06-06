f944f041c168e1610ba3dd2157c46aae431cf26a
RosslerGKBoquiren <142700435+RosslerGKBoquiren@users.noreply.github.com>
2024-06-05 20:30:35 -0400

    Add files via upload

---
+++ C#/week 3/ConsoleApps6/Program.cs
@@ -0,0 +1,24 @@
+﻿using ConsoleApps6;
+
+namespace ConsoleApp6
+{
+    internal class Program// class is a blueprint of an object and assumed as a datatype when called in an instance method
+                          // for example, object: car, it will have abilities such as drive, break or open window, and inheritance: A5
+    {
+        static void Main(string[] args)
+        {
+            Car audi = new Car("Audi", 260, "red");
+            audi.Details();
+            audi.Drive();
+            Car bmw = new Car("Bmw", 600);
+            bmw.Details();
+            bmw.Drive();
+            Console.WriteLine("Enter 1 to stop");
+            string userInput = Console.ReadLine();
+            if (userInput == "1")
+                audi.Stop();
+            else
+                Console.WriteLine("Car is still moving.");
+        }
+    }
+}
---
+++ C#/week 3/ConsoleApps6/car.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ConsoleApps6
+{
+    internal class Car
+    {
+        private string _name;
+        private int _hp;
+        private string _color;
+
+        // default constructor
+        public Car()
+        {
+            _name = "Car";
+            _hp = 0;
+            _color = "Black";
+        }
+
+        // partial specification constructor
+        public Car(string name, int hp = 0)
+        {
+            _name = name;
+            _hp = hp;
+            _color = "black";
+            Console.WriteLine("A car {0} was created", name);
+        }
+        //full specification constructor
+        public Car(string name, int hp, string color) 
+        {
+            _name = name;
+            _hp = hp;
+            _color = color;
+            Console.WriteLine("A car, {0}, was created", _name);    
+        }
+        public void Drive()
+        {
+            Console.WriteLine("***{0} is moving***", _name);
+        }
+        public void Stop()
+        {
+            Console.WriteLine("***{0} has stopped***", _name);
+        }
+        public void Details()
+        {
+            Console.WriteLine("Car with the name of {0}, color {1}, has a horsepower of {2}", _name, _color, _hp);
+        }
+    }
+}
