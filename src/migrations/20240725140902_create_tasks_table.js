ef4fc8cef7aa0cb24084aab16b8917871b14b5c5
RosslerGKBoquiren <142700435+RosslerGKBoquiren@users.noreply.github.com>
2024-07-25 22:25:43 -0400

    Add files via upload

---
+++ src/migrations/20240725140902_create_tasks_table.js
@@ -0,0 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+    return knex.schema.createTable('tasks', (table) => {
+        table.increments('id').primary();
+        table.string('title').notNullable();
+        table.text('description');
+        table.boolean('completed').defaultTo(false);
+        table.timestamps(true, true);
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+    return knex.schema.dropTable('tasks');
+};
+
