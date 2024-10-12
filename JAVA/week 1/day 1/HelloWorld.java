4ac45f58ef2230ff9f727c40751f47c95907b066
RosslerGKBoquiren <142700435+RosslerGKBoquiren@users.noreply.github.com>
2024-10-12 11:43:53 -0400

    Add files via upload

---
+++ JAVA/week 1/day 1/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+	// First method
+	public static void main (String arg[]) {
+		// void because we dont want to return any type
+		// static because we dont want to make any instance of the class
+
+		// for printing
+		System.out.println("Hello World"); // ln: means the print goes to the next line
+		
+	}
+
+}
